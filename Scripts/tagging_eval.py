fc2f83cf3511516dd22c1853551f2f93a8218f49
dereksalama <dereksalama@gmail.com>
2013-11-25 14:49:10 -0500

    Comment tagging_eval.py

--- Scripts/tagging_eval.py
+++ Scripts/tagging_eval.py
@@ -5,6 +5,36 @@ import sys
 import re
 from collections import defaultdict
 
+"""
+tagging_eval.py
+Derek Salama and Jake Leichtling
+
+This script determines the accuracy of a part-of-speech tagging,
+given the Viterbi output and gold-standard tagging for the same
+corpus.
+
+We align the HMM states with parts of speech by finding the part
+of speech each state cooccurs most frequently with. Once we have
+this association, we calculate the ratio of words that have been
+correctly tagged.
+
+This evalutaion, of course, requires that the Viterbi output be
+in the same order as the input. Because we cannot enforce this
+ordering in a distributed system, this script also sorts the
+Viterbi output by the "byte-offset" included at the start of
+each line.
+
+Usage:
+    python tagging_eval.py viterbi_output_file sorted_output_file gold_tagging_file
+    
+Where viterbi_output_file is the output of our Viterbi implmentation, sorted_output_file
+is an intermediate file where we will write the tagging in sorted order, and gold_tagging_file
+is the Penn Tree Bank gold-standard tags converted into the proper format by the
+conver_gold_tagging.py script.
+"""
+
+# Given the lines of input in (word,pos) format, creates an
+# ordered list of all the parts of speech tags.
 def parse_tagging(file_name):
     inputf = codecs.open(file_name, 'r', 'utf8')
 
@@ -16,11 +46,14 @@ def parse_tagging(file_name):
 
         for pair in line.split():
             m = re.match(r"\((.*),(.*)\)", pair)
-            tag = m.group(2)
+            tag = m.group(2) # just get the tag
             total_tagging.append(tag)
 
     return total_tagging
 
+# Given the ordered list of viterbi tags and gold-standard tags,
+# create a mapping between the two by finding the gold-standard
+# tag that each state coccures most frequently with
 def create_tag_map(hmm_tags, gold_tags):
     maximum_map = dict()
     for tag in set(hmm_tags): # iterate through unique tags
@@ -30,12 +63,15 @@ def create_tag_map(hmm_tags, gold_tags):
             if hmm_t == tag:
                 cooccurrence_count[gold_t] += 1
 
+        # Find the max by the cooccurence count
         maximum_cooccurrance = max(cooccurrence_count.iterkeys(),
                 key=(lambda key: cooccurrence_count[key]))
         maximum_map[tag] = maximum_cooccurrance
 
     return maximum_map
 
+# Given the hmm_to_gold mapping, and ordered viterbi and gold standard
+# output, simply count the number of times it is correct
 def num_correct(hmm_to_gold, hmm_tags, gold_tags):
     correct = 0
     for (hmm_t, gold_t) in zip(hmm_tags, gold_tags):
@@ -44,6 +80,9 @@ def num_correct(hmm_to_gold, hmm_tags, gold_tags):
 
     return correct
 
+# Sort the lines from the distributed algorithm by
+# the byte offset of those lines
+# (as was recorded in the original output)
 def sort_lines(file_name, output_name):
     inputf = codecs.open(file_name, 'r', 'utf8')
     outputf = codecs.open(output_name, 'w', 'utf8')
